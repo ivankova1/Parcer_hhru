a048c9b71720d8e4d85ac520f6a3efdd3a87cadc
ivan kova1 <ivan.2002aus@gmail.com>
2025-03-09 14:57:11 +0300

    -

--- api_heandler.py
+++ api_heandler.py
@@ -1,25 +1,26 @@
 import requests
 from typing import Dict, List
 
-class HHRUHandler:
+class HHAPIHandler:
     BASE_URL = "https://api.hh.ru"
 
     def get_companies_and_vacancies(self, company_ids: List[int]) -> Dict[str, List[Dict]]:
         """Получение данных о компаниях и их вакансиях"""
         companies_data = {}
+
         for company_id in company_ids:
             company_url = f"{self.BASE_URL}/employers/{company_id}"
             response = requests.get(company_url)
             if response.status_code == 200:
                 company_name = response.json().get("name")
-                vacancies_url = f"{self.BASE_URL}/vacancies?employer={company_id}"
+                vacancies_url = f"{self.BASE_URL}/vacancies?employer_id={company_id}"
                 vacancies_response = requests.get(vacancies_url)
-                if response.status_code == 200:
+                if vacancies_response.status_code == 200:
                     vacancies = [
                         {
                             "title": vac.get("name"),
                             "salary": vac.get("salary", {}).get("from"),
-                            "url": vac.get("altrnate_url")
+                            "url": vac.get("alternate_url")
                         }
                         for vac in vacancies_response.json().get("items", [])
                     ]
